b04e20435d6ba27cf4a8e87570c1c493e8f08dea
SaujanyaMagarde <saujanyamagarde5@gmail.com>
2025-06-16 00:04:52 +0530

    fixes7

--- ecommerce/src/Pages/LoginPage/login.jsx
+++ ecommerce/src/Pages/LoginPage/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Login.css";
+import "./login.css";
 import {useSelector,useDispatch} from 'react-redux'
 import {login,logout} from '../../ReduxStore/AuthSlice.jsx'
 import {Link} from 'react-router-dom'
