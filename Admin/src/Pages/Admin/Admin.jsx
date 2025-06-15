6d065eb17de3096726e0076ef7220037dda0b5a3
SaujanyaMagarde <saujanyamagarde5@gmail.com>
2025-06-15 23:56:01 +0530

    fixes4

--- Admin/src/Pages/Admin/Admin.jsx
+++ Admin/src/Pages/Admin/Admin.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Navbar from '../../Components/Navbar/Navbar.jsx';
-import Sidebar from '../../Components/Sidebar/Sidebar.jsx';
+import Sidebar from '../../Components/sidebar/Sidebar.jsx';
 import Profile from '../../Components/Profile/Profile.jsx';
 import './Admin.css';
 
